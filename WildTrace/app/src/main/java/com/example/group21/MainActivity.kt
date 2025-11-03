f39917175b8ddb46377725dad196a553e4762ace
Reid Lockhart <reidglockhart@gmail.com>
2025-11-03 10:00:16 -0800

    added some inputs to the signup view

--- WildTrace/app/src/main/java/com/example/group21/MainActivity.kt
+++ WildTrace/app/src/main/java/com/example/group21/MainActivity.kt
@@ -8,13 +8,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -43,7 +48,7 @@ fun AppNavigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "signup") {
         // --- Reid's Screens (Using Placeholders) ---
         composable("signup") {
-            SignUpView_Placeholder(navController)
+            signupView(navController)
         }
         composable("login") {
             LoginView_Placeholder(navController)
@@ -64,6 +69,55 @@ fun AppNavigation(navController: NavHostController) {
             )
         }
     }
+}
+
+@Composable
+fun signupView(navController: NavController) {
+    //
+    // Has access to the entered email and password
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        signupLoginInput(
+            labelText = "Username",
+            text = email,
+            onChange = { newText -> email = newText }
+        )
+        signupLoginInput(
+            labelText = "Password",
+            text = password,
+            onChange = { newText -> password = newText }
+        )
+    }
+}
+
+@Composable
+fun signupLoginInput(labelText: String, text: String, onChange: (String) -> Unit) {
+
+    val colorScheme = MaterialTheme.colorScheme
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = onChange,
+        label = { Text(labelText) },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = colorScheme.surface,
+            focusedIndicatorColor = colorScheme.primary,
+            focusedLabelColor = colorScheme.primary,
+            unfocusedContainerColor = colorScheme.primary.copy(alpha = 0.7f),
+            unfocusedIndicatorColor = colorScheme.primary.copy(alpha = 0.7f),
+            unfocusedLabelColor = colorScheme.primary.copy(alpha = 0.7f),
+            cursorColor = colorScheme.primary,
+        ),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    )
 }
 
 //Temporary Placeholders (can be removed later)
